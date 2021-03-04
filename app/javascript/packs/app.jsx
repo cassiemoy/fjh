6bcfd5434c73293e428dc29feea1e3eb2788961d
Cassie Moy <hello@cassie.nyc>
2021-03-03 21:49:01 -0500

    Add react-leaflet and example.

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -5,9 +5,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+const position = [51.505, -0.09]
 
 const App = () => (
-  <h1>hi</h1>
+  <h1>hi
+
+  <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+  </h1>
 )
 
 App.propTypes = {
